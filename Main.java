9e02bfe7f938c4de03ad99dd961858094ea88a18
Tamás Balla <egyszeru14@gmail.com>
2019-04-30 15:33:24 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package UI;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main (String[] args) {
+        MainFrame calc = new MainFrame();
+        calc.setVisible(true);
+
+        /**
+         * to-do
+         *
+         * uj muveletnel uj gomb
+         * ne lehessen betut beleirni
+         * font
+         * draggable
+         * ne nyiljon meg tobbszor a kis ablak
+         */
+
+
+
+
+
+
+    }
+
+}
