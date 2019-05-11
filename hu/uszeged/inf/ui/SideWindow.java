fcd62c6bb05922537978c900465bc7742dce20c4
excraptor <egyszeru14@gmail.com>
2019-05-11 12:57:21 +0200

    new button function

--- hu/uszeged/inf/ui/SideWindow.java
+++ hu/uszeged/inf/ui/SideWindow.java
@@ -10,50 +10,70 @@ import java.util.ArrayList;
 
 public class SideWindow {
 
+       private int sorszam = 0;
+      public void makeNewButton (String id, String showingID, GridBagConstraints gbc, JFrame frame) {
+              Button newButton = new Button(id, showingID);
+              gbc.gridy = sorszam/5+1;
+              gbc.gridx = sorszam%5;
+              frame.add(newButton, gbc);
+              sorszam++;
+       }
+
     public SideWindow (JTextField textField) {
 
 
 
            JFrame moreOperations = new JFrame();
-           moreOperations.setBounds(170, 200, 50, 700);
+           moreOperations.setBounds(170, 200, 100, 700);
            moreOperations.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-           moreOperations.setLayout(new GridLayout(2, 1));
+           GridBagLayout grid = new GridBagLayout();
+           GridBagConstraints gbc = new GridBagConstraints();
+           moreOperations.setLayout(grid);
+           gbc.fill = GridBagConstraints.BOTH;
+           gbc.weightx = 1.0;
+           gbc.weighty = 1.0;
            moreOperations.setUndecorated(true);
            moreOperations.setBackground(new Color(0x0));
            moreOperations.setForeground(new Color(0x0));
-           moreOperations.setShape(new RoundRectangle2D.Double(0, 0, 50, 700, 15, 15));
+           moreOperations.setShape(new RoundRectangle2D.Double(0, 0, 100, 700, 15, 15));
 
 
 
-           JButton close = new JButton("X");
+           /*JButton close = new JButton("X");
            close.setBackground(new Color(80, 0, 0));
            close.setForeground(Color.LIGHT_GRAY);
            close.addActionListener(e -> System.exit(0));
-           moreOperations.add(close);
-
-           File actual = new File("hu/uszeged/inf/ui/plugin");
-           ArrayList<String> files = new ArrayList<>();
-           for( File f : actual.listFiles()){
-                  files.add(f.getName());
-           }
+           moreOperations.add(close);*/
 
 
 
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
+           makeNewButton("asd", "asd", gbc, moreOperations);
 
-
-
-                  try {
-                         Class c = Class.forName("Sin");
-                         Field id = c.getField("id");
-                         id.setAccessible(true);
-                         Object value = id.get(new Object());
-                         System.out.println(value);
-                         /*Button newButton = new Button(id, showingID);
-                         moreOperations.add(newButton);*/
-
-                  } catch (Throwable e) {
-                         e.printStackTrace();
-                  }
 
 
 
